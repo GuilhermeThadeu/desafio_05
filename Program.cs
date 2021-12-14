213594f3802e9d37dcdad5f5538312aaf404be58
Guilherme Thadeu <guilhermetadeuh@hotmail.com>
2021-12-13 21:26:57 -0300

    criei um banco de dados de postgres no Dbeaver e conectei ele ao C# usando "connectionString = "Host=localhost;Username=postgres;Password=9630;Database=postgres";"
    Se conectar vai aparecer uma mensagem informando e se estiver errado o catch vai informar com uma mensagem de erro.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+using System;
+using System.Data.SqlClient;
+using Xceed.Wpf.Toolkit;
+
+namespace Desafio_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connectionString;
+                NpgsqlConnection connection;
+
+                connectionString = "Host=localhost;Username=postgres;Password=9630;Database=postgres";
+                connection = new NpgsqlConnection(connectionString);
+                connection.Open();
+                Console.WriteLine("Se conectou ao banco de dados!");
+                connection.Close();
+            }
+            catch (NpgsqlException)
+            {
+                Console.WriteLine("Erro ao se conectar");
+                
+            }
+
+        }
+
+
+    }
+}
